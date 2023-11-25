9308c3bc1b8260b00f64133bef568144a8487757
h3nc4 <me@h3nc4.com>
2023-11-25 11:55:17 -0300

    backtracking

---
+++ src/Backtracking.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Classe que implementa o algoritmo de Backtracking para distribuição de rotas
+ * entre caminhões
+ */
+public class Backtracking {
+    /** Array com as rotas a serem distribuídas */
+    private int[] rotas;
+    /** Lista com as rotas distribuídas */
+    private List<List<Integer>> atualRotas;
+    /** Lista com as melhores rotas distribuídas */
+    private List<List<Integer>> melhorRotas;
+    /** Número de caminhões disponíveis */
+    private int caminhoes;
+    /** Array com as distâncias atuais */
+    private int[] distbAtual;
+    /** Array com as melhores distâncias */
+    private int[] melhorDistrb;
+
+    /**
+     * Construtor da classe Backtracking
+     * 
+     * @param rotas        vetor com as rotas a serem distribuídas
+     * @param numCaminhoes número de caminhões disponíveis
+     */
+    public Backtracking(int[] rotas, int numCaminhoes) {
+        this.rotas = rotas;
+        this.caminhoes = numCaminhoes;
+        this.atualRotas = new LinkedList<>();
+        for (int i = 0; i < numCaminhoes; i++)
+            this.atualRotas.add(new LinkedList<>());
+        this.distbAtual = new int[numCaminhoes];
+        this.melhorDistrb = new int[numCaminhoes];
+        Arrays.fill(this.melhorDistrb, Integer.MAX_VALUE);
+    }
+
+    /**
+     * Método que distribui as rotas entre os caminhões
+     * 
+     * @return instância da classe Backtracking
+     */
+    public Backtracking distribuirRotas() {
+        return this.distribuir(0);
+    }
+
+    /**
+     * Método privado e recursivo que de facto distribui as rotas entre os caminhões
+     * 
+     * @param q índice da rota a ser distribuída
+     * @return instância da classe Backtracking
+     */
+    private Backtracking distribuir(int q) {
+        // Poda de distribuições inferiores
+        if (q == this.rotas.length) {
+            if (Arrays.stream(this.distbAtual).max().getAsInt() < Arrays.stream(this.melhorDistrb).max().getAsInt()) {
+                System.arraycopy(this.distbAtual, 0, this.melhorDistrb, 0, this.caminhoes);
+                melhorRotas = atualRotas.stream().map(ArrayList::new).collect(Collectors.toList());
+            }
+            return this;
+        }
+
+        for (int i = 0; i < this.caminhoes; i++) {
+            List<Integer> caminhao = this.atualRotas.get(i);
+            caminhao.add(this.rotas[q]);
+            this.distbAtual[i] += this.rotas[q];
+            this.distribuir(q + 1);
+            caminhao.removeIf(rota -> rota == this.rotas[q]);
+            this.distbAtual[i] -= this.rotas[q];
+        }
+        return this;
+    }
+
+    /**
+     * Método que imprime a melhor distribuição de rotas
+     */
+    public void print() {
+        for (int i = 0; i < this.caminhoes; i++) {
+            String rotas = this.melhorRotas.get(i).stream().map(Object::toString).collect(Collectors.joining(", "));
+            System.out.printf("Caminhão %d: rotas %s - total %dkm%n", i + 1, rotas, this.melhorDistrb[i]);
+        }
+    }
+}
