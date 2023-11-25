e6fbfc062bc3e4b6c897cb9817f66b2f500a2fa6
h3nc4 <me@h3nc4.com>
2023-11-25 13:03:43 -0300

    testes de tempo

---
+++ src/App.java
@@ -0,0 +1,38 @@
+/**
+ * Classe principal do projeto, usada para realizar os múltiplos casos de teste
+ */
+public class App {
+    private static void testar(int caminhoes, int inicio, int execucoes, long limiteTempo) {
+        int conjunto = inicio;
+        boolean lock = true;
+        while (lock) {
+            long tempoTotal = 0;
+                System.out.printf("%nTamanho %d: %n", conjunto);
+            for (int[] rotas : GeradorDeProblemas.geracaoDeRotas(conjunto, execucoes, 0.5)) {
+                long _inicio = System.currentTimeMillis();
+                new Backtracking(rotas, caminhoes).distribuirRotas().print();
+                long tempoExecucao = System.currentTimeMillis() - _inicio;
+                tempoTotal += tempoExecucao;
+                if (tempoExecucao > limiteTempo) {
+                    System.out.printf("%nTamanho %d ultrapassou o limite de tempo.%n", conjunto);
+                    lock = false;
+                    break; // Interrompe as execuções para esse tamanho
+                }
+            }
+
+            long mediaTempo = tempoTotal / execucoes;
+            System.out.printf("%nTamanho %d - Tempo Médio: %d ms%n", conjunto, mediaTempo);
+            System.out.println("--------------------------------------------------");
+            if (mediaTempo > limiteTempo) {
+                System.out.printf("%nTamanho %d ultrapassou o limite de tempo.%n", conjunto);
+                lock = false;
+                break; // Interrompe os testes
+            }
+            conjunto++;
+        }
+    }
+
+    public static void main(String[] args) {
+        testar(3, 6, 10, 30 * 1000);
+    }
+}
