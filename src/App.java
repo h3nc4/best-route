865c8b8febe64319e9b244bef8700e6db1069b76
h3nc4 <me@h3nc4.com>
2023-12-06 17:20:02 -0300

    app preparado para o requisito de 10T

--- src/App.java
+++ src/App.java
@@ -1,21 +1,27 @@
+import java.util.LinkedList;
+import java.util.List;
+
 import algoritmos.*;
 
 /**
  * Classe principal do projeto, usada para realizar os múltiplos casos de teste
  */
 public class App {
-    private static void testar(Distribuicao algoritmo, int caminhoes, int inicio, int execucoes, long limiteTempo) {
+    private static List<List<int[]>> testar(Distribuicao alg, int caminhoes, int inicio, int execucoes, long limite) {
         int conjunto = inicio;
         boolean lock = true;
+        List<List<int[]>> totalRotas = new LinkedList<>();
         while (lock) {
             long tempoTotal = 0;
             // System.out.printf("%nTamanho %d: %n", conjunto);
-            for (int[] rotas : GeradorDeProblemas.geracaoDeRotas(conjunto, execucoes, 0.5)) {
+            List<int[]> listaRotas = GeradorDeProblemas.geracaoDeRotas(conjunto, execucoes, 0.5);
+            totalRotas.add(listaRotas);
+            for (int[] rotas : listaRotas) {
                 long _inicio = System.currentTimeMillis();
-                algoritmo.distribuirRotas(rotas, caminhoes);
+                alg.distribuirRotas(rotas, caminhoes);
                 long tempoExecucao = System.currentTimeMillis() - _inicio;
                 tempoTotal += tempoExecucao;
-                if (tempoExecucao > limiteTempo) {
+                if (tempoExecucao > limite) {
                     System.out.printf("%nTamanho %d ultrapassou o limite de tempo.%n", conjunto);
                     lock = false;
                     break; // Interrompe as execuções para esse tamanho
@@ -23,19 +29,21 @@ public class App {
             }
 
             long mediaTempo = tempoTotal / execucoes;
-            System.out.printf("%nTamanho %d - Tempo Médio: %d ms%n", conjunto, mediaTempo);
+            if (lock)
+                System.out.printf("%nTamanho %d - Tempo Médio: %d ms%n", conjunto, mediaTempo);
             // System.out.println("--------------------------------------------------");
-            if (mediaTempo > limiteTempo) {
+            if (mediaTempo > limite) {
                 System.out.printf("%nTamanho %d ultrapassou o limite de tempo.%n", conjunto);
                 lock = false;
                 break; // Interrompe os testes
             }
             conjunto++;
         }
+        return totalRotas;
     }
 
     public static void main(String[] args) {
-        // testar(new Backtracking(), 3, 6, 10, 30 * 1000);
+        testar(new Backtracking(), 3, 6, 10, 30 * 1000);
 
         // testar(new GulosoOrdem(), 3, 6, 10, 30 * 1000);
 
