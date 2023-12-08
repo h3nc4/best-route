032e603a4a5b6831e3ac172173f763ef5773b0c7
h3nc4 <me@h3nc4.com>
2023-12-08 11:36:53 -0300

    pd solucionado

--- src/algoritmos/ProgDinamica.java
+++ src/algoritmos/ProgDinamica.java
@@ -12,9 +12,7 @@ import java.util.List;
  * caso contrário. Caso o valor chegue a ser verdadeiro na célula
  * T[<x>][<valor_aceitavel>], então as rotas verdadeiras são retiradas de T,
  * adicionadas a um caminhão e o processo é repetido até que todas as rotas
- * sejam distribuídas. Caso o valor chegue a ser falso na célula
- * T[<x>][<valor_aceitavel>], então ele será adicionado ao caminhão
- * com menor quilometragem média.
+ * sejam distribuídas.
  */
 public class ProgDinamica implements Distribuicao {
     /** Array com as rotas a serem distribuídas */
@@ -52,10 +50,30 @@ public class ProgDinamica implements Distribuicao {
     /**
      * Distribui as rotas entre os caminhões
      * 
+     * Chama o método distribuirAtual() e remove as rotas distribuídas do array de
+     * rotas, adicionando-as ao array de caminhões distribuídos
+     * 
+     * Cria um novo array de rotas com as rotas que não foram distribuídas
+     * 
      * @return A própria instância da classe
      */
     private ProgDinamica distribuir() {
-
+        for (int i = 0; i < this.caminhoesDistribuidos.length; i++) {
+            // Distribuir as rotas
+            int[] rotasDistribuidas = this.distribuirAtual();
+            // Adicionar as rotas distribuídas ao array de caminhões distribuídos
+            this.caminhoesDistribuidos[i] = rotasDistribuidas;
+            // Criar um novo array de rotas com as rotas que não foram distribuídas
+            this.rotas = Arrays.stream(this.rotas)
+                    .filter(r -> Arrays.stream(rotasDistribuidas).noneMatch(r2 -> r2 == r))
+                    .toArray();
+            // Criar uma nova tabela de soluções
+            if (this.caminhoesDistribuidos.length - i - 1 != 0) {
+                int aceitavel = Arrays.stream(this.rotas).sum() / (this.caminhoesDistribuidos.length - i - 1);
+                this.T = new boolean[this.rotas.length + 1][aceitavel + (int) (aceitavel * 0.1) + 1];
+            }
+        }
+        return this;
     }
 
     /**
@@ -65,11 +83,12 @@ public class ProgDinamica implements Distribuicao {
      */
     private int[] distribuirAtual() {
         // Inicializa a primeira linha da tabela
-        for (int j = 0; j < this.T[0].length; j++)
-            this.T[0][j] = true;
+        for (int j = 1; j < this.T[0].length; j++)
+            this.T[1][j] = false;
         // Inicializa a primeira coluna da tabela
-        for (int i = 1; i < this.T.length; i++)
-            this.T[i][0] = false;
+        for (int i = 0; i < this.T.length; i++)
+            this.T[i][0] = true;
+        this.printarTabela();
         // Preenche a tabela atual
         for (int i = 1; i < this.T.length; i++)
             for (int j = 1; j < this.T[i].length; j++) {
@@ -79,6 +98,8 @@ public class ProgDinamica implements Distribuicao {
                 // atual pode ser adicionada ao caminhão para preencher o valor atual
                         j >= this.rotas[i - 1] && this.T[i - 1][j - this.rotas[i - 1]];
             }
+
+        this.printarTabela();
 
         // Retornar as rotas distribuídas
         return this.coletar();
@@ -111,6 +132,21 @@ public class ProgDinamica implements Distribuicao {
     }
 
     /**
+     * Função de debug
+     * 
+     * Imprime a tabela de soluções
+     */
+    private void printarTabela() {
+        System.out.println("Tabela de soluções:");
+        for (int i = 0; i < this.T.length; i++) {
+            for (int j = 0; j < this.T[i].length; j++)
+                System.out.printf("%d ", this.T[i][j] ? 1 : 0);
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    /**
      * Imprime a melhor distribuição de rotas
      */
     public void print() {
@@ -124,7 +160,7 @@ public class ProgDinamica implements Distribuicao {
     }
 
     public static void main(String[] args) {
-        int[] rotas = { 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+        int[] rotas = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         int caminhoes = 3;
         new ProgDinamica().distribuirRotas(rotas, caminhoes);
     }
