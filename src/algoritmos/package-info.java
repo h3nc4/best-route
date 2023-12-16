16b6e4c46d79cebecef35880fbd5021f376f705c
h3nc4 <me@h3nc4.com>
2023-12-16 08:27:46 -0300

    última release

---
+++ src/algoritmos/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Este pacote contém as classes que implementam os algoritmos de ordenação.
+ */
+package algoritmos;
---
+++ src/app/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Este pacote contém a classe principal do projeto e o seu gerador de casos de
+ * teste.
+ */
+package app;
